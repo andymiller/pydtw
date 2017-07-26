62aa19b6aefca5152f62bf987d3a0d0d8abab894
Andrew Miller <andrew.colin.miller@gmail.com>
2017-07-26 09:10:30 -0700

    updated estup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+
+#from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
 setup(
+   name = "Andrew Miller",
+   license = "MIT",
+   packages = ['pydtw'],
    ext_modules  = cythonize(['pydtw/*.pyx']),
    include_dirs = [numpy.get_include(),],
 )
